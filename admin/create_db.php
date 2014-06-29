2a3d531d17f498a012a6625b45b15c3f27f18a0d
Scott Russell <scottedwardrussell@gmail.com>
2014-06-29 12:40:11 -0400

    edited create_db.php for include cred_ext.php statement

--- admin/create_db.php
+++ admin/create_db.php
@@ -1,23 +1,21 @@
 <?php
-DEFINE('DB_USERNAME', 'root');
-DEFINE('DB_PASSWORD', 'root');
-DEFINE('DB_HOST', 'localhost:3306');
+	include "cred_ext.php";
 
-//build connection to DB
-$con = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD);
+	//build connection to DB
+	$con = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD);
 
-//verify connection
-if(mysqli_connect_errno()){
-	echo "Failed to connect to MySQL:".mysqli_connect_error();
-}
+	//verify connection
+	if(mysqli_connect_errno()){
+		echo "Failed to connect to MySQL:".mysqli_connect_error();
+	}
 
-$sql = "CREATE DATABASE test_db";
-if(
-	mysqli_query($con,$sql)
-	){
-	echo "Database test_db created successfully. \n";
-}else{
-	echo "Error creating database: ".mysqli_error($con);
-}
-mysqli_close($con);
+	$sql = "CREATE DATABASE test_db";
+	if(
+		mysqli_query($con,$sql)
+		){
+		echo "Database test_db created successfully. \n";
+	}else{
+		echo "Error creating database: ".mysqli_error($con);
+	}
+	mysqli_close($con);
 ?>
