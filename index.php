31855674e0c559b53e6689f93100a201e840bf34
Scott Russell <scottedwardrussell@gmail.com>
2014-06-29 12:12:36 -0400

    Modified index.php remove extra links

--- index.php
+++ index.php
@@ -3,22 +3,19 @@
  <body>
  <h1>A100 Application Portal</h1>
  <p>Returning Applicants please enter your email address and pass phrase</p>
- <p>credentials</p>
 <div>
 	<form action="resources/existing_applicant.php" method="post">
 	Email: <input type="text" name="emailLogin">
 	<br>
 	Password: <input type="password" name="passwordLogin">
 	</br><input type="submit" name="login" value="login">
-	<a href="resources/existing_applicant.php">Hello</a>
 </form>
 </div>
 <div>
 	<p><a href="resources/new_applicant.php">New Applicants Click Here</a></p>
 </div>
 
-<p>Admin Functionality</p>
-<p><a href="resources/select.php">Click here to view table</a></p>
+<h2>Admin Functionality</h2>
 <?php include 'resources/select.php' ?>
  </body>
 </html>
